f2a40519b5a05b1dbbafb4193048433173ee46a6
Rafael Xavier <rxaviermontero@gmail.com>
2021-10-31 21:49:34 -0300

    Some rework

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import streamlit as st
 from pyech import ECH
 
-
 st.set_page_config(page_title="PyECH")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+.css-12oz5g7 {padding-top: 2rem;}
+</style>
+
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+categorical_dict = {"Auto": None, "Categórica": True, "No categórica": False}
+function_dict = {"Suma": "sum", "Promedio": "mean", "Recuento": "count"}
 
 #@st.cache(show_spinner=False)
 def load_survey(year, weights):
@@ -13,32 +25,40 @@ def load_survey(year, weights):
 
 st.sidebar.image("https://github.com/CPA-Analytics/pyech/raw/master/logo.png")
 st.sidebar.markdown("PyECH es un procesador de la Encuesta Continua de Hogares del INE escrito en Python.")
-st.sidebar.markdown("Aunque fue diseñado para ser utilizado como librería, esta app incorpora la funcionalidad básica.")
-year_select = st.sidebar.selectbox(label="Seleccionar año de encuesta", options=list(range(2006, 2021)), index=13)
-weights_text = st.sidebar.text_input("Seleccionar ponderador", value="pesoano")
-load_button = st.sidebar.button("Cargar", on_click=load_survey, args=(year_select, weights_text))
-
+st.sidebar.markdown("Esta webapp representa la funcionalidad más básica de PyECH, pero que a priori abraca la mayoría de los análisis fundamentales que se pueden hacer con la Encuesta Continua de Hogares: cruzar variables.")
+st.sidebar.markdown("El paquete de Python sobre el cual esta app está construida también permite calcular percentiles, convertir variables a precios constantes y dólares, y en general brinda una interfaz flexible para trabajar con la encuesta. [Este notebook en Google Colab](https://colab.research.google.com/github/CPA-Analytics/pyech/blob/master/examples/example.ipynb) recorre la API de PyECH y muestra su funcionalidad")
+st.sidebar.markdown("---")
+st.sidebar.markdown("""
+                    * [Github](https://github.com/CPA-Analytics/pyech)
+                    * [PyPI](https://pypi.org/project/pyech/)
+                    * [Documentación](https://pyech.readthedocs.io/en/latest/)
+                    """)
+st.title("PyECH")
+st.caption("Procesamiento de la ECH del INE en Python.")
+section = st.selectbox("Sección", options=["Carga", "Resumir variables", "Explorar diccionario"], index=0)
 if "ech" in st.session_state:
-    st.text(f"Encuesta cargada: {st.session_state.ech.data['anio'][0]} | Ponderador: {st.session_state.ech.weights}")
-section = st.selectbox("Sección", options=["Resumir variables", "Explorar diccionario", "Sobre PyECH"])
+    st.markdown(f"**Encuesta cargada**: {st.session_state.ech.data['anio'][0]} | **Ponderador**: {st.session_state.ech.weights}")
 st.markdown("---")
 
-if section == "Explorar diccionario" and "ech" in st.session_state:
-    #st.subheader("Explorar diccionario")
-    term = st.text_input("Términos de búsqueda", value="ingreso")
-    n_elements = st.slider("Cantidad de elementos", min_value=1, value=10)
+if section == "Carga":
+    col1, col2 = st.columns(2)
+    year_select = col1.selectbox(label="Seleccionar año de encuesta", options=list(range(2006, 2021)), index=13)
+    weights_text = col2.text_input("Seleccionar ponderador", value="pesoano")
+    load_button = st.button("Cargar", on_click=load_survey, args=(year_select, weights_text))
+
+elif section == "Explorar diccionario" and "ech" in st.session_state:
+    col1, col2 = st.columns(2)
+    term = col1.text_input("Términos de búsqueda", value="ingreso")
+    n_elements = col2.slider("Cantidad de elementos", min_value=1, value=10)
     st.dataframe(st.session_state.ech.search_dictionary(term=term).head(n_elements).astype(str))
 
-categorical_dict = {"Auto": None, "Categórica": True, "No categórica": False}
-function_dict = {"Suma": "sum", "Promedio": "mean", "Recuento": "count"}
-
-if section == "Resumir variables" and "ech" in st.session_state:
-    #st.subheader("Resumir variables")
-    variable_select = st.selectbox("Seleccionar variable", options=st.session_state.ech.data.columns)
-    group_select = st.multiselect("Seleccionar agrupadores", options=st.session_state.ech.data.columns)
-    function_select = st.selectbox("Seleccionar agregador", options=["Suma", "Promedio", "Recuento"])
-    st.caption("Si la variable es categórica solo se agregará mediante recuento.")
-    categorical_select = st.selectbox("Tipo de variable", options=["Auto", "Categórica", "No categórica"], index=0)
+elif section == "Resumir variables" and "ech" in st.session_state:
+    col1, col2 = st.columns(2)
+    variable_select = col1.selectbox("Seleccionar variable", options=st.session_state.ech.data.columns)
+    group_select = col2.multiselect("Seleccionar agrupadores", options=st.session_state.ech.data.columns)
+    function_select = col1.selectbox("Seleccionar operación", options=["Suma", "Promedio", "Recuento"])
+    col1.caption("Si la variable es categórica solo se agregará mediante recuento.")
+    categorical_select = col2.selectbox("Definir tipo de variable", options=["Auto", "Categórica", "No categórica"], index=0)
     try:
         summ = st.session_state.ech.summarize(variable_select, by=group_select, is_categorical=categorical_dict[categorical_select],
                                 aggfunc=function_dict[function_select], variable_labels=True)
@@ -47,10 +67,5 @@ if section == "Resumir variables" and "ech" in st.session_state:
     except:
         pass
 
-if section == "Sobre PyECH":
-    st.markdown("Esta app representa la funcionalidad más básica de PyECH, pero que a priori abraca la mayoría de los análisis fundamentales que se pueden hacer con la Encuesta Continua de Hogares: cruzar variables.")
-    st.markdown("El paquete de Python sobre el cual esta app está construida también permite calcular percentiles, convertir variables a precios constantes y dólares, y en general brinda una interfaz flexible para trabajar con la encuesta. [Este notebook en Google Colab](https://colab.research.google.com/github/CPA-Analytics/pyech/blob/master/examples/example.ipynb) recorre la API de PyECH y muestra su funcionalidad")
-    st.markdown("---")
-    st.markdown("[Github](https://github.com/CPA-Analytics/pyech)")
-    st.markdown("[PyPI](https://pypi.org/project/pyech/)")
-    st.markdown("[Documentación](https://pyech.readthedocs.io/en/latest/)")
+else:
+    st.markdown("Una encuesta tiene que ser cargada en la sección de _Carga_ para poder resumir variables o explorar el diccionario.")
